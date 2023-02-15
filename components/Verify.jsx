6d7bf58a7eeba2fa03f05e1cb2c593c924bb31ca
AtulJoshyy <atuljoshyy@gmail.com>
2023-02-15 10:58:39 +0530

    commit

---
+++ components/Verify.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+
+const verify = () => {
+    const [vaccine, setVaccine] = useState('')
+    const [isverified, setIsverified] = useState(false)
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        setIsverified(true)
+    }
+    return (
+
+        <>
+            <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-lg text-center">
+                    <h1 className="text-2xl font-bold sm:text-3xl">Verify Vaccine Details</h1>
+                </div>
+
+                <form action="" onSubmit={handleFormSubmit} className="mx-auto mt-8 mb-0 max-w-md space-y-4">
+                    <div>
+                        <label for="email" className="sr-only">name</label>
+
+                        <div className="relative">
+                            <input
+                                type="text"
+                                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                                placeholder="Enter name"
+                            />
+
+                            <span className="absolute inset-y-0 right-4 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                </svg>
+
+                            </span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="password" className="sr-only">vaccine name</label>
+                        <div className="relative">
+                            <input
+                                type="text"
+                                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                                placeholder="Enter vaccine name"
+                            />
+
+                            <span className="absolute inset-y-0 right-4 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                                </svg>
+
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <button
+                            type="submit"
+                            className="ml-3 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+                        >
+                            Verify user
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            {isverified && (<article
+                className="flex justify-center w-1/4 mx-auto items-center gap-4 rounded-lg border border-gray-400 bg-white p-6"
+            >
+                <span className="rounded-full bg-blue-100 p-3 text-blue-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                    </svg>
+
+                </span>
+
+                <div>
+                    <p className="text-2xl font-medium text-gray-900">Verified</p>
+                </div>
+            </article>
+            )}
+        </>
+
+    )
+}
+
+export default verify
